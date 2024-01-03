4180db392969b50ba97e9e57fb6eef12c1f0403a
Rhubarb06150 <matheogass@outlook.com>
2024-01-03 21:34:00 +0100

    version mario

---
+++ scripts.js
@@ -0,0 +1,69 @@
+function smw(){
+  document.getElementById("logo_img").src="images/logos/logo_smw.png";
+  document.getElementById("logo_img").style.height="66px";
+  document.getElementById("logo_img").style.width="498px";
+  document.getElementById("hills_bg").style.backgroundImage = "url(images/bg/hills_smw.png)";
+  document.getElementById("hills_bg").style.height = "262px";
+  document.getElementById("clouds_bg").style.backgroundImage = "url(images/bg/clouds_smw.png)";
+  document.getElementById("clouds_bg").style.height= "542px";
+  document.getElementById("footer_bg").style.backgroundImage = "url(images/bg/bottom_smw.png)";
+  document.getElementById("top_id").style.borderImage = "url(images/bg/cloud_top_smw.png) 32 repeat";
+  document.getElementById("top_id").style.backgroundColor = "rgb(240,240,240)";
+  const boxes = document.querySelectorAll('.box_blue');
+  boxes.forEach(box=>{
+    box.style.borderImage = "url(images/box/box_smw_1.png) 32 repeat";
+    box.style.backgroundImage = "url(images/box/mid_smw_1.png)"
+  });
+  const boxes_ = document.querySelectorAll('.box_red');
+  boxes_.forEach(box=>{
+    box.style.borderImage = "url(images/box/box_smw_2.png) 32 repeat";
+    box.style.backgroundImage = "url(images/box/mid_smw_2.png)"
+  });
+  }
+function smb3(){
+  document.getElementById("logo_img").src="images/logos/logo.png";
+  document.getElementById("logo_img").style.height="70px";
+  document.getElementById("logo_img").style.width="542px";
+  document.getElementById("hills_bg").style.backgroundImage = "url(images/bg/bg.png)";
+  document.getElementById("hills_bg").style.height = "300px";
+  document.getElementById("clouds_bg").style.backgroundImage = "url(images/bg/clouds.png)";
+  document.getElementById("clouds_bg").style.height = "532px";
+  document.getElementById("clouds_bg").style.bottom = "100px";
+  document.getElementById("footer_bg").style.backgroundImage = "url(images/bg/bottom.png)";
+  document.getElementById("top_id").style.borderImage = "url(images/bg/cloud_top.png) 32 repeat";
+  document.getElementById("top_id").style.backgroundColor = "rgb(248,248,248)";
+  const boxes = document.querySelectorAll('.box_blue');
+  boxes.forEach(box=>{
+    box.style.borderImage = "url(images/box/border_blue.png) 32 repeat";
+    box.style.backgroundImage = "url(images/box/mid_blue.png)"
+  });
+  const boxes_ = document.querySelectorAll('.box_red');
+  boxes_.forEach(box=>{
+    box.style.borderImage = "url(images/box/border_red.png) 32 repeat";
+    box.style.backgroundImage = "url(images/box/mid_red.png)"
+  });
+}
+
+function smb(){
+  document.getElementById("logo_img").src="images/logos/logo_smb.png";
+  document.getElementById("logo_img").style.height="80px";
+  document.getElementById("logo_img").style.width="418px";
+  document.getElementById("hills_bg").style.backgroundImage = "url(images/bg/hills_smb.png)";
+  document.getElementById("hills_bg").style.height = "432px";
+  document.getElementById("clouds_bg").style.backgroundImage = "url(images/bg/clouds_smb.png)";
+  document.getElementById("clouds_bg").style.height = "478px";
+  document.getElementById("clouds_bg").style.bottom = "96px";
+  document.getElementById("footer_bg").style.backgroundImage = "url(images/bg/bottom_smb.png)";
+  document.getElementById("top_id").style.borderImage = "url(images/bg/cloud_top_smb.png) 32 repeat";
+  document.getElementById("top_id").style.backgroundColor = "rgb(255,255,255)";
+  const boxes = document.querySelectorAll('.box_blue');
+  boxes.forEach(box=>{
+    box.style.borderImage = "url(images/box/box_smb_1.png) 32 repeat";
+    box.style.backgroundImage = "url(images/box/mid_smb_1.png)"
+  });
+  const boxes_ = document.querySelectorAll('.box_red');
+  boxes_.forEach(box=>{
+    box.style.borderImage = "url(images/box/box_smb_2.png) 32 repeat";
+    box.style.backgroundImage = "url(images/box/mid_smb_2.png)"
+  });
+}
