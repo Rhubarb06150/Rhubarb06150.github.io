bde6b1c198423a415409fb4048270128c0dcdad8
Rhubarb06150 <matheogass@outlook.com>
2024-01-04 18:47:56 +0100

    box size auto adjust

--- scripts.js
+++ scripts.js
@@ -1,52 +1,39 @@
-document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
-window.addEventListener("load", pageFullyLoaded, false);
-
-function theDomHasLoaded(e) {
-    ui_start();
-    load_acc_index();
-}
-
-function pageFullyLoaded(e) {
-  ui_start();
-  load_acc_index();
-}
-
-function smw(){
-  document.getElementById("logo_img").src="https://mcrhubarb.net/images/logos/logo_smw.png";
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    document.getElementById("logo_img").style.height="33px";
-    document.getElementById("logo_img").style.width="249px";
-  }else{
-    document.getElementById("logo_img").style.height="66px";
-    document.getElementById("logo_img").style.width="498px";
+function smw() {
+  document.getElementById("logo_img").src = "https://mcrhubarb.net/images/logos/logo_smw.png";
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.getElementById("logo_img").style.height = "33px";
+    document.getElementById("logo_img").style.width = "249px";
+  } else {
+    document.getElementById("logo_img").style.height = "66px";
+    document.getElementById("logo_img").style.width = "498px";
   }
   document.getElementById("hills_bg").style.backgroundImage = "url(https://mcrhubarb.net/images/bg/hills_smw.png)";
   document.getElementById("hills_bg").style.height = "262px";
   document.getElementById("clouds_bg").style.backgroundImage = "url(https://mcrhubarb.net/images/bg/clouds_smw.png)";
-  document.getElementById("clouds_bg").style.height= "542px";
+  document.getElementById("clouds_bg").style.height = "542px";
   document.getElementById("footer_bg").style.backgroundImage = "url(https://mcrhubarb.net/images/bg/bottom_smw.png)";
   document.getElementById("top_id").style.borderImage = "url(https://mcrhubarb.net/images/bg/cloud_top_smw.png) 32 repeat";
   document.getElementById("top_id").style.backgroundColor = "rgb(240,240,240)";
   const boxes = document.querySelectorAll('.box_blue');
-  boxes.forEach(box=>{
+  boxes.forEach(box => {
     box.style.borderImage = "url(https://mcrhubarb.net/images/box/box_smw_1.png) 32 repeat";
     box.style.backgroundImage = "url(https://mcrhubarb.net/images/box/mid_smw_1.png)"
   });
   const boxes_ = document.querySelectorAll('.box_red');
-  boxes_.forEach(box=>{
+  boxes_.forEach(box => {
     box.style.borderImage = "url(https://mcrhubarb.net/images/box/box_smw_2.png) 32 repeat";
     box.style.backgroundImage = "url(https://mcrhubarb.net/images/box/mid_smw_2.png)"
   });
   localStorage.setItem('ui', 'smw')
-  }
-function smb3(){
-  document.getElementById("logo_img").src="https://mcrhubarb.net/images/logos/logo.png";
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    document.getElementById("logo_img").style.height="35px";
-    document.getElementById("logo_img").style.width="271px";
-  }else{
-    document.getElementById("logo_img").style.height="70px";
-    document.getElementById("logo_img").style.width="542px";
+}
+function smb3() {
+  document.getElementById("logo_img").src = "https://mcrhubarb.net/images/logos/logo.png";
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.getElementById("logo_img").style.height = "35px";
+    document.getElementById("logo_img").style.width = "271px";
+  } else {
+    document.getElementById("logo_img").style.height = "70px";
+    document.getElementById("logo_img").style.width = "542px";
   }
   document.getElementById("hills_bg").style.backgroundImage = "url(https://mcrhubarb.net/images/bg/bg.png)";
   document.getElementById("hills_bg").style.height = "300px";
@@ -57,29 +44,29 @@ function smb3(){
   document.getElementById("top_id").style.borderImage = "url(https://mcrhubarb.net/images/bg/cloud_top.png) 32 repeat";
   document.getElementById("top_id").style.backgroundColor = "rgb(248,248,248)";
   const boxes = document.querySelectorAll('.box_blue');
-  boxes.forEach(box=>{
+  boxes.forEach(box => {
     box.style.borderImage = "url(https://mcrhubarb.net/images/box/border_blue.png) 32 repeat";
     box.style.backgroundImage = "url(https://mcrhubarb.net/images/box/mid_blue.png)"
   });
   const boxes_ = document.querySelectorAll('.box_red');
-  boxes_.forEach(box=>{
+  boxes_.forEach(box => {
     box.style.borderImage = "url(https://mcrhubarb.net/images/box/border_red.png) 32 repeat";
     box.style.backgroundImage = "url(https://mcrhubarb.net/images/box/mid_red.png)"
   });
   localStorage.setItem('ui', 'smb3')
 }
 
-function smb(){
-  document.getElementById("logo_img").src="https://mcrhubarb.net/images/logos/logo_smb.png";
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    document.getElementById("logo_img").style.height="40px";
-    document.getElementById("logo_img").style.width="209px";
-  }else{
-    document.getElementById("logo_img").style.height="80px";
-    document.getElementById("logo_img").style.width="418px";
+function smb() {
+  document.getElementById("logo_img").src = "https://mcrhubarb.net/images/logos/logo_smb.png";
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.getElementById("logo_img").style.height = "40px";
+    document.getElementById("logo_img").style.width = "209px";
+  } else {
+    document.getElementById("logo_img").style.height = "80px";
+    document.getElementById("logo_img").style.width = "418px";
   }
-  document.getElementById("logo_img").style.height="80px";
-  document.getElementById("logo_img").style.width="418px";
+  document.getElementById("logo_img").style.height = "80px";
+  document.getElementById("logo_img").style.width = "418px";
   document.getElementById("hills_bg").style.backgroundImage = "url(https://mcrhubarb.net/images/bg/hills_smb.png)";
   document.getElementById("hills_bg").style.height = "432px";
   document.getElementById("clouds_bg").style.backgroundImage = "url(https://mcrhubarb.net/images/bg/clouds_smb.png)";
@@ -89,25 +76,25 @@ function smb(){
   document.getElementById("top_id").style.borderImage = "url(https://mcrhubarb.net/images/bg/cloud_top_smb.png) 32 repeat";
   document.getElementById("top_id").style.backgroundColor = "rgb(255,255,255)";
   const boxes = document.querySelectorAll('.box_blue');
-  boxes.forEach(box=>{
+  boxes.forEach(box => {
     box.style.borderImage = "url(https://mcrhubarb.net/images/box/box_smb_1.png) 32 repeat";
     box.style.backgroundImage = "url(https://mcrhubarb.net/images/box/mid_smb_1.png)"
   });
   const boxes_ = document.querySelectorAll('.box_red');
-  boxes_.forEach(box=>{
+  boxes_.forEach(box => {
     box.style.borderImage = "url(https://mcrhubarb.net/images/box/box_smb_2.png) 32 repeat";
     box.style.backgroundImage = "url(https://mcrhubarb.net/images/box/mid_smb_2.png)"
   });
   localStorage.setItem('ui', 'smb')
 }
 
-function ui_start(){
+function ui_start() {
   var x = localStorage.getItem('ui')
-  if (x == 'smw'){
+  if (x == 'smw') {
     smw()
-  } else if (x == 'smb'){
+  } else if (x == 'smb') {
     smb()
-  } else{
+  } else {
     smb3()
   }
 }
@@ -125,64 +112,132 @@ function getBase64Image(img) {
   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
-function upload_acc(){
+function upload_acc() {
   const photo_sel = document.getElementById("selectAvatar");
   var username = document.getElementById('username').value;
-  if (username.length < 3){
-  document.getElementById("username_alert").style.display = "flex";
+  if (username.length < 3) {
+    document.getElementById("username_alert").style.display = "flex";
   } else {
-    localStorage.setItem('username',username);
+    localStorage.setItem('username', username);
     var pfp_img = document.getElementById("pfp_pr").src;
-    localStorage.setItem('profile_picture',pfp_img)
+    localStorage.setItem('profile_picture', pfp_img)
     document.getElementById("pref_up").style.display = "flex";
     document.getElementById("username_alert").style.display = "none";
   }
 }
 
-function load_acc(){
+function load_acc() {
   var username = localStorage.getItem('username')
   var pfp = localStorage.getItem('profile_picture')
-  document.getElementById('username').value= username;
-  if (pfp==null){
+  document.getElementById('username').value = username;
+  if (pfp == null) {
     document.getElementById("pfp_pr").src = '../images/head/default.png';
-  }else{
+  } else {
     document.getElementById("pfp_pr").src = pfp;
   }
 }
 
-function load_acc_index(){
+function load_acc_index() {
   var username = localStorage.getItem('username')
   var pfp = localStorage.getItem('profile_picture')
-  if (username == null){
+  if (username == null) {
     document.getElementById('profile_name').innerHTML = 'No profile';
-  }else{
+  } else {
     document.getElementById('profile_name').innerHTML = username;
   }
-  if (pfp==null){
+  if (pfp == null) {
     document.getElementById("pfp_pr_index").src = 'images/head/default.png';
-  }else{
+  } else {
     document.getElementById("pfp_pr_index").src = pfp;
   }
 }
 
 function readURL(input) {
   if (input.files && input.files[0]) {
-  
+
     var reader = new FileReader();
-    reader.onload = function (e) { 
-      document.querySelector("#pfp_pr").setAttribute("src",e.target.result);
+    reader.onload = function (e) {
+      document.querySelector("#pfp_pr").setAttribute("src", e.target.result);
     };
 
-    reader.readAsDataURL(input.files[0]); 
+    reader.readAsDataURL(input.files[0]);
   }
 }
 
-function check_height(){
-  var hei = document.getElementById("profile_box").clientHeight;
-  document.getElementById('profile_name').innerHTML = hei;
-  while ((hei % 64) != 0) {
-    hei++;
-  }
-  document.getElementById('profile_box').style.height = hei;
-  document.getElementById('profile_name').innerHTML = hei;
+function check_dim() {
+  const red_box = document.querySelectorAll("div.box_red");
+  red_box.forEach(box => {
+    var hei = box.clientHeight;
+    while ((hei % 32) != 0) {
+      hei++;
+    }
+    hei = (hei + 32).toString() + "px";
+    box.style.height = hei;
+    var wid = box.clientWidth;
+    while ((wid % 32) != 0) {
+      wid++;
+    }
+    wid = (wid + 32).toString() + "px";
+    box.style.width = wid;
+  });
+  const blue_box = document.querySelectorAll("div.box_blue");
+  blue_box.forEach(box => {
+    var hei = box.clientHeight;
+    while ((hei % 32) != 0) {
+      hei++;
+    }
+    hei = (hei + 32).toString() + "px";
+    box.style.height = hei;
+    var wid = box.clientWidth;
+    while ((wid % 32) != 0) {
+      wid++;
+    }
+    wid = (wid + 32).toString() + "px";
+    box.style.width = wid;
+  });
+  const orange_box = document.querySelectorAll("div.box_orange");
+  orange_box.forEach(box => {
+    var hei = box.clientHeight;
+    while ((hei % 32) != 0) {
+      hei++;
+    }
+    hei = (hei + 32).toString() + "px";
+    box.style.height = hei;
+    var wid = box.clientWidth;
+    while ((wid % 32) != 0) {
+      wid++;
+    }
+    wid = (wid + 32).toString() + "px";
+    box.style.width = wid;
+  });
+  const white_box = document.querySelectorAll("div.box_white");
+  white_box.forEach(box => {
+    var hei = box.clientHeight;
+    while ((hei % 32) != 0) {
+      hei++;
+    }
+    hei = (hei + 32).toString() + "px";
+    box.style.height = hei;
+    var wid = box.clientWidth;
+    while ((wid % 32) != 0) {
+      wid++;
+    }
+    wid = (wid + 32).toString() + "px";
+    box.style.width = wid;
+  });
+  const green_box = document.querySelectorAll("div.box_green");
+  green_box.forEach(box => {
+    var hei = box.clientHeight;
+    while ((hei % 32) != 0) {
+      hei++;
+    }
+    hei = (hei + 32).toString() + "px";
+    box.style.height = hei;
+    var wid = box.clientWidth;
+    while ((wid % 32) != 0) {
+      wid++;
+    }
+    wid = (wid + 32).toString() + "px";
+    box.style.width = wid;
+  });
 }
