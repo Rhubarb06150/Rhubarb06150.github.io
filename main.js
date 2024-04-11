10fb2dc5790341d29a3e3fe1d647c652ea6a6e02
Rhubarb06150 <matheogass@outlook.com>
2024-04-11 22:12:22 +0200

    test

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function AddElement(title,content){
-    div=document.createElement("div");
-    div.classList="element";
+function AddElement(title, content) {
+    div = document.createElement("div");
+    div.classList = "element";
 
-    title_div=document.createElement("div");
-    title_div.classList="title";
-    title_span=document.createElement("span");
+    title_div = document.createElement("div");
+    title_div.classList = "title";
+    title_span = document.createElement("span");
     title_span.innerHTML = title
     title_div.appendChild(title_span);
 
-    content_div=document.createElement("div");
-    content_div.classList="element_content";
-    content_content=document.createElement("p");
+    content_div = document.createElement("div");
+    content_div.classList = "element_content";
+    content_content = document.createElement("p");
     content_content.innerHTML = content;
     content_div.appendChild(content_content);
 
