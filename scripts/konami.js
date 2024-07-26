a86e11197979252072f094fcb7be7f2fec49294e
Rhubarb06150 <matheogass@outlook.com>
2024-07-26 15:39:53 +0200

    push

---
+++ scripts/konami.js
@@ -0,0 +1,28 @@
+var allowedKeys={37:'left',38:'up',39:'right',40:'down',65:'a',66:'b'};
+var konamiCode=['up','up','down','down','left','right','left','right','b','a'];
+var konamiCodePosition=0;
+let bonus=new Audio("/files/rhb.mp3");
+document.addEventListener('keydown',function(e){
+var key=allowedKeys[e.keyCode];
+var requiredKey=konamiCode[konamiCodePosition];
+if (key==requiredKey){
+konamiCodePosition++;
+if (konamiCodePosition==konamiCode.length){
+bonus.currentTime=0;
+bonus.play();
+console.log("Salut Rhubarb :D");
+document.getElementById('rhb-bg').removeAttribute("hidden");
+konamiCodePosition=0;
+divs=document.querySelectorAll("p,h1");
+for(const div of divs){
+if(div.id!="rhb-bg"&&div.id!="box"){
+div.style.animation="maite-anim "+Math.floor(Math.random()*30)/10+"s infinite";
+};
+};
+maite=document.getElementById('maite').src="/files/objs/rhb-app.webp";
+};
+}else{
+konamiCodePosition = 0;
+}
+});
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,5 @@
 function aB(img,link,color_t,color_b,text){
 let button=document.createElement('div');
-/*button.addEventListener("mouseover",function(){
-button.style.boxShadow=color_t+" 0px 0px 40px";
-});
-button.addEventListener("mouseout",function(){
-button.style.boxShadow="none";
-});*/
 button.addEventListener("click",function(){open(link)});
 button.classList="button";
 button.style.background="linear-gradient(180deg, "+color_t+" 0%,"+color_b+" 100%)";
@@ -23,17 +17,3 @@ button.appendChild(img_div);
 button.appendChild(p_link);
 document.getElementById('main').appendChild(button);
 };
-/*
-function slideMinia(from,to,direction){
-let ind_minia_to;
-let ind_minia_from=minia_index;
-let minia_to=document.getElementById('minia-to');
-let minia_from=document.getElementById('minia-from');
-minia_to.src="minias/pr"+minias[ind_minia_to]+".webp";
-minia_from.src="/minias/pr"+minias[ind_minia_from]+".webp";
-if(direction="left"){
-if(minia_index==0){ind_minia_to=minias.length;};
-}else{
-}
-};
-*/
